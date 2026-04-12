c9f6e22fca9b8f562fbb331ef383391364fe370e
locxl <sokx126@gmail.com>
2026-04-12 23:40:07 +0800

    feat(font): 修改网站字体为霞鹭文楷

--- src/config.ts
+++ src/config.ts
@@ -213,16 +213,16 @@ export const siteConfig: SiteConfig = {
 		asciiFont: {
 			// 英文字体 - 优先级最高
 			// 指定为英文字体则无论字体包含多大范围，都只会保留 ASCII 字符子集
-			fontFamily: "msyh",
+			fontFamily: "LXGWWenKai-Regular",
 			fontWeight: "500",
-			localFonts: ["msyh.ttf"],
+			localFonts: ["LXGWWenKai-Regular.ttf"],
 			enableCompress: true, // 启用字体子集优化，减少字体文件大小
 		},
 		cjkFont: {
 			// 中日韩字体 - 作为回退字体
-			fontFamily: "msyh",
+			fontFamily: "LXGWWenKai-Regular",
 			fontWeight: "500",
-			localFonts: ["msyh.ttf"],
+			localFonts: ["LXGWWenKai-Regular.ttf"],
 			enableCompress: true, // 启用字体子集优化，减少字体文件大小
 		},
 	},
