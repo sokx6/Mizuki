48d7ab441b5eb05fced1a0a8b869e8fb8a98c261
locxl <sokx126@gmail.com>
2026-06-19 16:37:30 +0800

    fix: 修改缩放宽度

--- src/config.ts
+++ src/config.ts
@@ -67,7 +67,7 @@ export const siteConfig: SiteConfig = {
 	// 页面自动缩放配置
 	pageScaling: {
 		enable: true, // 是否开启自动缩放
-		targetWidth: 2000, // 目标宽度，低于此宽度时开始缩放
+		targetWidth: 1700, // 目标宽度，低于此宽度时开始缩放
 	},
 
 	bangumi: {
